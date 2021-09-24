b293b51f7ef19d892682f6d27372a5f15267363c
Traian <traian.plosca@gmail.com>
2021-09-24 11:31:25 +0300

    finished CustomerMain.kt

--- app/src/main/java/com/example/refood/CustomerMain.kt
+++ app/src/main/java/com/example/refood/CustomerMain.kt
@@ -1,14 +1,24 @@
 package com.example.refood
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.view.LayoutInflater
-import android.widget.ImageButton
+import android.widget.*
+import android.widget.Toast
+
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+import android.content.pm.PackageManager
+import android.os.Build
+
+import androidx.annotation.NonNull
+import androidx.annotation.RequiresApi
 
 
 /**
@@ -16,6 +26,7 @@ import android.widget.ImageButton
  */
 class CustomerMain : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.customer_main)
@@ -42,6 +53,41 @@ class CustomerMain : AppCompatActivity() {
 
             dialog.show()
         }
+
+        val settings = findViewById<ImageView>(R.id.settings)
+        settings.setOnClickListener {
+            val alertDialog = AlertDialog.Builder(this, android.R.style.Theme_Light_NoTitleBar_Fullscreen)
+
+            val inflater: LayoutInflater =
+                this.layoutInflater // receive the inflater to create the view
+
+            val dialogView: View =
+                inflater.inflate(R.layout.dialog_rewards, null) // create the view
+
+            val closeDialogButton = dialogView.findViewById<ImageButton>(R.id.close_dialog_button)
+            val listView = dialogView.findViewById<ListView>(R.id.dialog_item_selection)
+            val title = dialogView.findViewById<TextView>(R.id.selection_text_dialog)
+            listView.visibility = View.GONE
+            title.text = "Settings"
+
+            alertDialog.setView(dialogView) // set the view
+
+            val dialog: AlertDialog = alertDialog.create()
+            closeDialogButton.setOnClickListener { dialog.dismiss() }
+
+            dialog.show()
+        }
+
+        val scanBtn = findViewById<Button>(R.id.scan_btn)
+        scanBtn.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(Array<String>(1){Manifest.permission.CAMERA}, 100)
+            } else {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivity(intent)
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -53,5 +99,22 @@ class CustomerMain : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show()
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, 1888)
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
